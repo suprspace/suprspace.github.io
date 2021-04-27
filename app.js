44ddf0aa26a2365452516540504c49790b4ac227
suprspace <83039102+suprspace@users.noreply.github.com>
2021-04-27 15:51:07 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res) {
+  console.log('request was made: ' + req.url);
+  res.writeHead(200, {'Content-Type': 'index/html'});
+  var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+  myReadStream.pipe(res);
+});
